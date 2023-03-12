8da6b2ef9d5553284af9acebe489c9f723c68254
narayani2327 <harikalanarayani@gmail.com>
2023-03-12 16:36:40 +0530

    typing effect

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,26 @@
 import Data from "./Data";
-
+import React, { useEffect } from "react";
+import { gsap, TimelineMax } from 'gsap';
 function About() {
+    useEffect(()=>{
+        var tl = new TimelineMax({
+            paused:true
+        });
+        tl.fromTo(".title", 5, {
+            width: "0",
+        }, {
+            width: "9.50em",
+            ease:"none"
+        }, 0);
+        tl.fromTo(".title", 0.5, {
+            "border-right-color": "rgba(255,255,255,0.75)"
+        }, {
+            "border-right-color": "rgba(255,255,255,0)",
+            repeat: -1,
+            ease:"none"
+        }, 0);
+        tl.play();
+    },[])
     return (
         <div className="about">
             <p className="title">WHY SELLULAR?</p>    
