871666f70322462f73969354b7503114e3ab2df1
narayani2327 <harikalanarayani@gmail.com>
2023-03-15 15:58:29 +0530

    events animation

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import { gsap } from 'gsap';
+import React, { useRef, useEffect } from "react";
 function displayDiv(){
     document.querySelector(".hack").style.display = "none";
     document.querySelector(".hackdays").style.display = "none";
     document.querySelector(".days").style.display = "none";
 }
 function Events() {
+    let hackv=useRef(null)
+    let hackdaysv=useRef(null)
+    let daysv=useRef(null)
     useEffect(()=>{
         var hack=document.getElementById("hack")
         var hackdays=document.getElementById("hackdays")
@@ -14,7 +18,11 @@ function Events() {
             hackdays.classList.remove("active")
             days.classList.remove("active")
             displayDiv()
-            document.querySelector(".hack").style.display = "block";
+            document.querySelector(".hack").style.display = "block"
+            gsap.to(hackv,{
+                x:1200,
+                duration:1
+            })
         });
         hackdays.addEventListener("click",()=>{
             hackdays.classList.add("active")
@@ -22,6 +30,14 @@ function Events() {
             days.classList.remove("active")
             displayDiv()
             document.querySelector(".hackdays").style.display = "block";
+            gsap.to(hackv,{
+                x:-1200,
+                duration:1
+            })
+            gsap.to(daysv,{
+                x:1200,
+                duration:1
+            })
         });
         days.addEventListener("click",()=>{
             days.classList.add("active")
@@ -29,6 +45,10 @@ function Events() {
             hack.classList.remove("active")
             displayDiv()
             document.querySelector(".days").style.display = "block";
+            gsap.to(daysv,{
+                x:-1200,
+                duration:1
+            })
         });
     },[])
     return (
@@ -40,15 +60,15 @@ function Events() {
                     <li  id="days">days</li>
                 </ul>
             </div>
-            <div className="hack">
+            <div className="hack" ref={(el=>hackv=el)}>
                 <p>HAck Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati placeat distinctio veniam porro perferendis neorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati placeat distinctio veniam porro perferendis neque repudiandae veritatis quia illum desque repudiandae veritatis quia illum deserunt!</p>
                 <a href="#">Know More</a>
             </div>
-            <div className="hackdays active-div">
+            <div className="hackdays active-div" ref={(el=>hackdaysv=el)}>
                 <p>hackdays Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati placeat distinctio veniam porro perferendis neque repudiandae veritatis quia illum orem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati placeat distinctio veniam porro perferendis neque repudiandae veritatis quia illum desdeserunt!</p>
                 <a href="#">Know More</a>
             </div>
-            <div className="days">
+            <div className="days" ref={(el=>daysv=el)}>
                 <p>Days Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati placeorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati placeat distinctio veniam porro perferendis neque repudiandae veritatis quia illum desat distinctio veniam porro perferendis neque repudiandae veritatis quia illum deserunt!</p>
                 <a href="#">Know More</a>
             </div>
